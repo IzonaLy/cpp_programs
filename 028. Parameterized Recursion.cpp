0f7d354ba30771be83b7cc1b60b53d47e3852121
IzonaLy <183250012+IzonaLy@users.noreply.github.com>
2024-10-19 17:26:56 +0530

    Create 028. Parameterized Recursion.cpp

---
+++ 028. Parameterized Recursion.cpp
@@ -0,0 +1,23 @@
+//Recursion With Extra Paramenter
+#include<iostream>
+using namespace std;
+void print(int n,int m=0){
+    if (m==n){
+        cout<<n<<endl;
+    } else {
+        cout<<m<<endl;
+        print(n,m+1);
+    }
+    
+    
+}
+int main(){
+    int n;
+    cout<<"Enter The Value of N";
+    cin>>n;
+    print(n);
+        
+    
+    return 0;
+    
+}
