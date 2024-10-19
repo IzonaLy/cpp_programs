974b3997d87711865a9b250f4f2dcbb5b47b49b2
IzonaLy <183250012+IzonaLy@users.noreply.github.com>
2024-10-20 01:42:12 +0530

    Create 044. Ways of Initializzing 2D Arrays.cpp

---
+++ 044. Ways of Initializzing 2D Arrays.cpp
@@ -0,0 +1,11 @@
+//Initialization of 2-D Arrays
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[4][2]= {{1,2},{3,4},{5,6},{7,8}};
+    int arr[4][2]= {1,2,3,4,5,6,7,8};
+    int arr[][3]= {1,2,3,4,5,6,7,8};
+    
+    return 0;
+}
