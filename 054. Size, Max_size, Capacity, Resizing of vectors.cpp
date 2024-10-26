789a234d8e78e957b32e1882d536ff658fb18e22
IzonaLy <183250012+IzonaLy@users.noreply.github.com>
2024-10-26 08:51:29 +0000

    Update and rename 054. Size, Max_size and Capacity of vectors.cpp to 054. Size, Max_size, Capacity, Resizing of vectors.cpp

--- 054. Size, Max_size, Capacity, Resizing of vectors.cpp
+++ 054. Size, Max_size, Capacity, Resizing of vectors.cpp
@@ -10,7 +10,15 @@ int main(){
   cout<<"The size of the vector is "<<vowels.size()<<endl;
   cout<<"The maximum_size of the vector is "<<vowels.max_size()<<endl;
   cout<<"The capacity of the vector is "<<vowels.capacity()<<endl;
-  
+  vowels.resize (3);//All Extra Elements are Erased
+  for (int i=0;i<vowels.size();i++){
+    cout<<vowels[i]<<endl;
+  }
+  vector<int> numbers={1,2,3,4};
+  numbers.resize(10);//Eztra Elements are added and filled with 0
+  for (int i=0;i<numbers.size();i++){
+    cout<<numbers[i]<<endl;
+  }
  
   return 0;
 }
